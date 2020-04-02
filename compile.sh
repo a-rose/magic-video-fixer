eb5cb73ca9170c564975b49454288a5ccf3ebb20
Antoine Rose <antoou@gmail.com>
2020-04-02 16:39:20 +0200

    feat(compile.sh): make the build type easier to change

--- compile.sh
+++ compile.sh
@@ -4,8 +4,9 @@
 
 BUILD_DIR="build"
 INSTALL_DIR=".."
+BUILD_TYPE=Release
 
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_INSTALL_RPATH="${BUILD_DIR}/lib"
+cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_INSTALL_RPATH="${BUILD_DIR}/lib"
 make -j4 && echo -e "\nStart with ./${BUILD_DIR}/magic-video-fixer"
